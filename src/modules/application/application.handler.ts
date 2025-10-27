c4e2c992a8680af27fb0e10f061fbd965ae3e985
Max-Hirning <maxhirning25@gmail.com>
2025-10-27 17:03:03 +0100

    fix: application health checker

---
+++ src/modules/application/application.handler.ts
@@ -0,0 +1,24 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+import { addDIResolverName } from "@/lib/awilix/awilix.js";
+import { ApplicationService } from "./application.service.js";
+
+export type ApplicationHandler = {
+    healthChecker: (
+        request: FastifyRequest,
+        reply: FastifyReply
+    ) => Promise<void>;
+};
+
+export const createApplicationHandler = (
+    applicationService: ApplicationService
+): ApplicationHandler => {
+    return {
+        healthChecker: async (_request, reply) => {
+            const data = await applicationService.healthChecker();
+
+            return reply.send(data);
+        },
+    };
+};
+
+addDIResolverName(createApplicationHandler, "applicationHandler");
